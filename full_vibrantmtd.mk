a917cfdd60d2eced808070c9fb8f977dc7616eba
Humberto Borba <humberos@gmail.com>
2014-11-27 22:42:14 -0200

    vibrantmtd: use the correct config for screen density
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I0b47d3dfb842701f908ee7ff9b2053490622dfbf

--- full_vibrantmtd.mk
+++ full_vibrantmtd.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit Omni GSM telephony parts
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
