60e02912a8e61ff036b8d16f7fcc0e47361fe2b2
sooti <sooti85@gmail.com>
2015-09-09 09:56:06 +0200

    vibrantmtd: use prebuilt kernel
    Change-Id: I8508782222ea6e34b10b5b08c39d1c7733c0c11a
    
    Conflicts:
    	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,8 @@
 #
 
 # Kernel Config
-TARGET_KERNEL_CONFIG := cyanogenmod_vibrantmtd_defconfig
+# TARGET_KERNEL_CONFIG := cyanogenmod_vibrantmtd_defconfig
+TARGET_PREBUILT_KERNEL := device/samsung/vibrant/boot.img
 
 # Recovery
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/vibrantmtd/recovery/recovery_keys.c
